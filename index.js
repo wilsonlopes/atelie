2408132b2fdeb8d76e15dbc43602940318408511
Eliezer Marques <neto15.cg@gmail.com>
2021-09-25 20:44:12 -0300

    cria funçoes de proximo e anterior da galeria

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const imagem1 = "images/99.png"
+const imagens = ["images/99.png", "images/88.png", "images/77.png"];
+let posicaoImagemAtual = 0;
+
+
+function vaiParaAnterior () {
+    const tamanhoDoArrayDeImagens = imagens.length;
+    const posicaoDaUltimaImagem = tamanhoDoArrayDeImagens - 1
+
+    if (posicaoImagemAtual === 0) {
+        document.getElementById("imagem1").src=imagens[posicaoDaUltimaImagem];
+        posicaoImagemAtual = posicaoDaUltimaImagem;
+    } else {
+        document.getElementById("imagem1").src=imagens[posicaoImagemAtual - 1];
+        posicaoImagemAtual = posicaoImagemAtual - 1;
+    }
+}
+
+function vaiParaProximo () {
+    const tamanhoDoArrayDeImagens = imagens.length;
+    const posicaoDaUltimaImagem = tamanhoDoArrayDeImagens - 1
+
+    if (posicaoImagemAtual === posicaoDaUltimaImagem) {
+        document.getElementById("imagem1").src = imagens[0];
+        posicaoImagemAtual = 0;
+    } else {
+        document.getElementById("imagem1").src = imagens[posicaoImagemAtual + 1];
+        posicaoImagemAtual = posicaoImagemAtual + 1;
+    }
+}
